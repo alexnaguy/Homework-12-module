23efbb70519887958372ffe2a22dd7e4550e28a7
AlexNaguy <gorniynaguy@mail.ru>
2023-08-05 11:05:04 +0300

    Выполнил задание 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 from prettytable import from_db_cursor
+from sql_request import *
 
 
 def create_database(path: str) -> sqlite3.Connection:
@@ -32,10 +33,13 @@ def execute_application():
     #Создание таблиц
     name_table_sellers = 'sellers'
     sql_request_sellers = create_request_sellers_or_buyer(name_table_sellers)
-    name_table_buyer = 'buyer'
+
+    name_table_buyer = 'buyers'
     sql_request_buyer = create_request_sellers_or_buyer(name_table_buyer)
+
     name_table_sales = 'sales'
     sql_request_sales = create_request_sales(name_table_sales)
+
     try:
         create_table(cursor, sql_request_sellers, name_table_sellers)
         create_table(cursor, sql_request_buyer, name_table_buyer)
@@ -45,5 +49,5 @@ def execute_application():
     finally:
         cursor.close()
         connection.close()
-        print(f'Соединение с базой данных {name_db} завершено.')
+        print(f'Соединение  завершено.')
 
