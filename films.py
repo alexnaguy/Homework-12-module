e8835287ef1231862b4e2a5a0d6a1f2506cd2a44
AlexNaguy <gorniynaguy@mail.ru>
2023-08-14 17:11:29 +0300

    Создал класс Films с модулем peewee

---
+++ films.py
@@ -0,0 +1,25 @@
+from peewee import *
+
+db = SqliteDatabase('films.db')
+
+
+class Films(Model):
+
+    class Meta:
+        database = db
+
+    title = CharField()
+    director = CharField()
+    year = CharField()
+    description = TextField()
+    actors = CharField()
+    duration = CharField
+
+
+    def __str__(self):
+        return f"{self.title} \n" \
+               f"{self.director} \n" \
+               f"{self.year} \n" \
+               f"{self.description}" \
+               f"{self.actors}" \
+               f"{self.duration}"\
