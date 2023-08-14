3a643609ad3f6ca1307a00f68333feca5711f5f0
AlexNaguy <gorniynaguy@mail.ru>
2023-08-14 17:21:30 +0300

    Создад класс Model

---
+++ model.py
@@ -0,0 +1,22 @@
+from films import  Films
+from datetime import date
+
+class Model:
+    """ Отвечает за внутреннюю логику работы программы.
+    Здесь мы можем скрыть способы хранения данных, а также правила и алгоритмы обработки информации."""
+
+    @staticmethod
+    def get_films_from_db():
+        films = Films.select().where(Films.year == "2023")
+        return films
+
+    @staticmethod
+    def add_films(film: tuple):
+        title, director, year, description, actors, duration = film
+        Films.create(title = title,
+                     director = director,
+                     year = year,
+                     description = description,
+                     actors = actors,
+                     duration = duration
+                     )
