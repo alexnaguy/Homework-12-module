ce6593e543e171448b44f124aea891c6677890bb
AlexNaguy <gorniynaguy@mail.ru>
2023-08-15 09:58:28 +0300

    Создал и реализвал класс Controller

---
+++ controller.py
@@ -0,0 +1,27 @@
+from receipe import Recipe
+from model import Model
+
+
+class Controller:
+    """
+    Обеспечивает связь между Моделью и действиями пользователя в результате взаимодействия с Представлением.
+    Принимает большинство решений о переходах приложения из одного состояния в другое.
+    Также выполняет валидацию данных, если она проходит, то данные передаются в Model.
+    """
+    __view = None
+
+    def __init__(self, model):
+        self.__model = model
+
+    def set_view(self, view):
+        self.__view = view
+
+    def get_recipe(self) -> None:
+        # Запрос данных из Модели
+        data = self.__model.get_recipe_from_db()
+        # Отдать данные на представление
+        self.__view.output_films(data)
+
+    def add_recipe(self, recipe: tuple):
+        # Запрос на добавление статьи
+        self.__model.add_recipe(recipe)
---
+++ view.py
@@ -0,0 +1,42 @@
+from films import Films, db
+import datetime
+from model import Model
+from controller import Controller
+
+
+
+class View:
+    """
+    Отвечает за отображение данных Модели.
+    На этом уровне мы лишь предоставляем интерфейс для взаимодействия пользователя с Моделью.
+
+    """
+    def __init__(self, model, controller):
+        self.__model = model
+        self.__controller = controller
+        self.__controller.set_view(self)
+        db.connect()
+
+    @staticmethod
+    def output_recipe(recipess):
+        for item in recipess:
+            print(item)
+
+    def main(self):
+
+        """ Сценарий 1. Вывести пользователю все статьи подряд """
+        #self.__controller.get_films()
+        """ Сценарий 2. Добавить новую статью """
+
+        title = "Оно",
+        director = "Крэзи До",
+        year = "2020",
+        description = "Очень страшное кино",
+        actors = "Актеры очень много",
+        duration = "2 часа"
+        film = (title, director, year, description, actors, duration)
+        self.__controller.add_film(film)
+
+
+    def __del__(self):
+        db.close()
